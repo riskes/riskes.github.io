7a8358c1781f7e7cae083b16cb7de0abfb59c2a6
Arnau Rísquez Guiteras <arisquezg@gmail.com>
2024-09-22 17:35:08 +0200

    actualitzacio

---
+++ Pages/ModelDeJoc.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebApp.Pages;
+
+[Authorize]
+public class ModelDeJoc : PageModel
+{
+    private readonly ILogger<ModelDeJoc> _logger;
+
+    public ModelDeJoc(ILogger<ModelDeJoc> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
